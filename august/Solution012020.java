d55cae2f6caec0883eb2905ab97ebaf890b78cbb
tattarush <tattarush@gmail.com>
2025-08-05 16:16:40 +0300

    inputstream

---
+++ august/Solution012020.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Solution012020 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("C:\\githowto\\data.txt");
+        long sum = 0;
+        while (inputStream.available() > 0) {
+            int data = inputStream.read();
+            sum+= data;
+        }
+        inputStream.close();
+        System.out.println(sum);
+    }
+
+}
