0a87dc7d60a46d88638d6537ac8f01ca89805ebd
tattarush <tattarush@gmail.com>
2025-08-05 17:07:07 +0300

    readbyte and find max

---
+++ august/iobyte.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/*
+Максимальный байт
+*/
+
+public class iobyte {
+    public static void main(String[] args) throws Exception {
+        List<Integer> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+
+        while (inputStream.available() > 0) {
+            list.add(inputStream.read());
+        }
+        inputStream.close();
+        Integer max = list.stream()
+                .max((i1, i2) -> i1 - i2)
+                .orElse(0);
+        System.out.println(max);
+    }
+}
