46a3b36f79ccb1a670894c05b97fa09d2161da0a
tattarush <tattarush@gmail.com>
2025-08-12 14:49:22 +0300

    half file

--- august/SolutionBytes.java
+++ august/SolutionBytes.java
@@ -6,23 +6,22 @@ public class SolutionBytes {
     public static void main(String[] args) throws IOException {
 
 
-        FileInputStream inputStream = new FileInputStream("august/resources/testSource/file1.txt");
-        FileOutputStream outputStream2 = new FileOutputStream("august/resources/testSource/file2.txt");
-        FileOutputStream outputStream3 = new FileOutputStream("august/resources/testSource/file3.txt");
+        FileInputStream inputStream = new FileInputStream("resources/testSource/file1.txt");
+        FileOutputStream outputStream2 = new FileOutputStream("resources/testSource/file2.txt");
+        FileOutputStream outputStream3 = new FileOutputStream("resources/testSource/file3.txt");
 
         if (inputStream.available() > 0) {
             byte[] buffer = new byte[inputStream.available()];
             int count = inputStream.read(buffer);
-//            for (int i = 0; i < buffer.length; i++) {
-//                System.out.println(buffer[i]);
-//            }
             if (count % 2 == 0) {
-                outputStream2.write(buffer, 0, (count / 2));
-                System.out.println(buffer[11]);
-                outputStream3.write(buffer, 0, count);
+                int half = count /2;
+                outputStream2.write(buffer, 0, half);
+                outputStream3.write(buffer, half, half);
             } else {
-                outputStream2.write(buffer, 0, ((count + 1) / 2));
-                outputStream3.write(buffer,((count + 1) / 2), count);
+                int firstHalf = (count + 1) /2;
+                int secondHalf = count - firstHalf;
+                outputStream2.write(buffer, 0, firstHalf);
+                outputStream3.write(buffer,firstHalf, secondHalf);
             }
 
         }
