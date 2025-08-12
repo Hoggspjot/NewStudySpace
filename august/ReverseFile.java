b84cd6a0312431ed1cdf9ddb9a42a79afe1196cd
tattarush <tattarush@gmail.com>
2025-08-12 16:10:52 +0300

    reverse более красиво

--- august/ReverseFile.java
+++ august/ReverseFile.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ReverseFile {
     public static void main(String[] args) throws IOException {
@@ -7,15 +10,13 @@ public class ReverseFile {
         FileInputStream inputStream = new FileInputStream("resources/reverse/sourceFile.txt");
         FileOutputStream outputStream = new FileOutputStream("resources/reverse/copyFile.txt");
 
+        List<Integer> readBytes = new ArrayList<>();
         while (inputStream.available() > 0) {
-            byte[] buffer = new byte[inputStream.available()];
-            int count = inputStream.read(buffer);
-            byte[] revert = new byte[buffer.length];
-            for (int i = 0, j = buffer.length - 1; i < buffer.length && j >= 0 ; i++, j--) {
-                revert[j] = buffer[i];
-            }
-            outputStream.write(revert, 0, count);
-
+            readBytes.add(inputStream.read());
+        }
+        Collections.reverse(readBytes);
+        for (Integer bytes : readBytes) {
+            outputStream.write(bytes);
         }
 
 
