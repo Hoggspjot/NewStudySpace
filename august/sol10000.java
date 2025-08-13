b4857c4296876db21abc5cdcc5b7f7a35d1eadbb
tattarush <tattarush@gmail.com>
2025-08-13 17:49:41 +0300

    reverse более красиво

---
+++ august/sol10000.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class sol10000 {
+    public static void main(String[] args) throws DownloadException {
+
+
+        try (FileInputStream inputStream = new FileInputStream("resources/bigFiles/big.txt")) {
+            int length = inputStream.available();
+            System.out.println(length);
+            byte[] buffer = new byte[inputStream.available()];
+            System.out.println(buffer.length);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
